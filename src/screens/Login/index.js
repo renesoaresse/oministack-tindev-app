112b027fa0a2e5f60f66f79c51428d0cae6f9ac8
Renê Soares <reenerochase@gmail.com>
2019-11-17 20:16:08 -0300

    feat(screen): Init screen

---
+++ src/screens/Login/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+import styles from './styles';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Login</Text>
+      </SafeAreaView>
+    );
+  }
+}
