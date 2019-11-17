0c5b962c3dc862486b8c646342fdb6e7219a9620
Renê Soares <reenerochase@gmail.com>
2019-11-17 20:59:42 -0300

    refactor(screen): Improving screen accessibility.

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 
-import {View, Image, TextInput, TouchableOpacity, Text} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Platform,
+} from 'react-native';
 
 import logo from '../../assets/logo.png';
 import styles from './styles';
@@ -8,10 +15,15 @@ import styles from './styles';
 export default class Login extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        enabled={Platform.OS === 'ios'}
+        style={styles.container}>
         <Image source={logo} />
 
         <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
           placeholder="Digite seu usuário no GitHub."
           style={styles.input}
           placeholderTextColor="#999"
@@ -20,7 +32,7 @@ export default class Login extends Component {
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Entrar</Text>
         </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
