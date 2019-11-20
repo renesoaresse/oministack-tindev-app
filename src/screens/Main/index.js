9fc8ff8fcf72932a47e5b7a6ba940a18bd3a8f34
Renê Soares <reenerochase@gmail.com>
2019-11-20 11:43:22 -0300

    feet(screen): Creating visual page components without styles and buttons.

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {SafeAreaView, Image, View, Text} from 'react-native';
+
+import logo from '../../assets/logo.png';
+import styles from './styles';
 
 export default function Main({navigation}) {
   return (
-    <View>
-      <Text>MAIN</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} />
+      <View style={styles.cardsContainer}>
+        <View style={styles.card}>
+          <Image
+            source={{
+              uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4',
+            }}
+          />
+          <View style={styles.footer}>
+            <Text style={styles.name}>Diego Fernandes</Text>
+            <Text style={styles.bio}>
+              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
+              desenvolvimento web e mobile.
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View />
+    </SafeAreaView>
   );
 }
--- src/screens/Main/styles.js
+++ src/screens/Main/styles.js
@@ -1,5 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
 
 export default styles;
