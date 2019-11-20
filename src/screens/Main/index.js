ef58f8155966fd0f0be24c827f897b4b7942b577
Renê Soares <reenerochase@gmail.com>
2019-11-20 11:55:58 -0300

    style(screen): Created the stylization of the card and logo.

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -8,17 +8,18 @@ import styles from './styles';
 export default function Main({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={logo} />
+      <Image style={styles.logo} source={logo} />
       <View style={styles.cardsContainer}>
         <View style={styles.card}>
           <Image
+            style={styles.avatar}
             source={{
               uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4',
             }}
           />
           <View style={styles.footer}>
             <Text style={styles.name}>Diego Fernandes</Text>
-            <Text style={styles.bio}>
+            <Text style={styles.bio} numberOfLines={2}>
               CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
               desenvolvimento web e mobile.
             </Text>
--- src/screens/Main/styles.js
+++ src/screens/Main/styles.js
@@ -7,6 +7,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  logo: {
+    marginTop: 30,
+  },
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  avatar: {
+    flex: 1,
+    height: 300,
+  },
+  footer: {
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  bio: {
+    fontSize: 14,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 18,
+  },
 });
 
 export default styles;
